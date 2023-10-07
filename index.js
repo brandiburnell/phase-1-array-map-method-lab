91dd20db06fcc0c20307af883b44b48bdb5c1d97
Brandi Burnell <brandiburnell6@gmail.com>
2023-10-07 11:11:16 -0600

    passed all tests

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  // iterate through array
+  let casedTutorials = tutorials.map(title => {
+    // Split title into an array of individual words
+    let titleWords = title.split(" ");
+    // capitalize the first letter of each word
+    let capitalizedTitleWords = titleWords.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    // Combine capitalized words back into one array
+    let combinedCapitalizedWords = capitalizedTitleWords.join(" ");
+    return combinedCapitalizedWords;
+  })
+
+  return casedTutorials;
 }
+
+
+
+
+// const titleCased = () => {
+
+//   // iterate through array
+
+//     const oneTitle = tutorials[0].split(" ");
+//     console.log(oneTitle);
+//     const capitalizedOneTitle = oneTitle.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     console.log(capitalizedOneTitle);
+//     const joinedCapitalizedTitle = capitalizedOneTitle.join(" ");
+//     console.log(joinedCapitalizedTitle);
+
+//   //return casedTutorials;
+// }
